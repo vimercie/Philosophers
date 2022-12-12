35970177dda869586698b1efb3a665c3c479f399
vimercie <vincent.mrc@hotmail.com>
2022-12-12 01:55:20 +0100

    deleted .gitignore

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: vimercie <vimercie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 18:40:54 by vimercie          #+#    #+#             */
-/*   Updated: 2022/12/06 00:06:53 by vimercie         ###   ########.fr       */
+/*   Updated: 2022/12/12 01:53:43 by vimercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,31 @@ int	free_data(t_data *data)
 	return (0);
 }
 
+int	fork_init(t_data *data, int i)
+{
+		data->p[i].left_fork = &data->forks_id[i];
+		if (pthread_mutex_init(&data->forks_id[i], NULL) != 0)
+			return (0);
+		if (data->args.n_philo > 1)
+		{
+			if (i < data->args.n_philo - 1)
+			{
+				data->p[i].right_fork = &data->forks_id[i + 1];
+				if (pthread_mutex_init(&data->forks_id[i + 1], NULL) != 0)
+					return (0);
+			}
+			else
+			{
+				data->p[i].right_fork = &data->forks_id[0];
+				if (pthread_mutex_init(&data->forks_id[0], NULL) != 0)
+					return (0);
+			}
+		}
+		else
+			data->p[i].right_fork = NULL;
+		return (1);
+}
+
 int	philo_init(t_data *data)
 {
 	int	i;
@@ -41,16 +66,19 @@ int	philo_init(t_data *data)
 	i = 0;
 	while (i < data->args.n_philo)
 	{
+		if (!fork_init(data, i))
+			return (0);
 		data->p[i].philo_id = malloc(sizeof(int));
 		data->p[i].n_eat = malloc(sizeof(int));
+		if (data->p[i].philo_id == NULL
+			|| data->p[i].n_eat == NULL)
+			{
+				free_philo(&data->p[i]);
+				return (0);
+			}
 		data->p[i].data = data;
 		*data->p[i].philo_id = i + 1;
 		*data->p[i].n_eat = data->args.n_eat;
-		data->p[i].left_fork = &data->forks_id[i];
-		if (i + 1 < data->args.n_philo)
-			data->p[i].right_fork = &data->forks_id[i + 1];
-		else
-			data->p[i].right_fork = &data->forks_id[0];
 		data->p[i].time.time_from_start = &data->time_from_start;
 		gettimeofday(&data->p[i].time.time_now, NULL);
 		i++;
@@ -73,15 +101,12 @@ int	data_init(t_data *data)
 		return (0);
 	}
 	data->i = 0;
-	while (data->i < data->args.n_philo)
-	{
-		pthread_mutex_init(&data->forks_id[data->i], NULL);
-		data->i++;
-	}
 	*data->stop = 0;
 	gettimeofday(&data->time_from_start, NULL);
-	data->i = 0;
 	if (!philo_init(data))
+	{
+		free_data(data);
 		return (0);
+	}
 	return (1);
 }
